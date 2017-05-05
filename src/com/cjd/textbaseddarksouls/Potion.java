2fac3b3aaa94f1cf4e4a13e8afcca6e40401869a
Duncan X. Simpson <virtualdxs@gmail.com>
2017-05-05 16:58:31 -0700

    Set up potion interface

--- src/com/cjd/textbaseddarksouls/Potion.java
+++ src/com/cjd/textbaseddarksouls/Potion.java
@@ -2,7 +2,6 @@ package com.cjd.textbaseddarksouls;
 
 import static com.cjd.textbaseddarksouls.DarkSoulsMaster.*; //Give access to master context
 
-//Not yet implemented
-public abstract class Potion{
-
+public interface Potion{
+  void drink();
 }
