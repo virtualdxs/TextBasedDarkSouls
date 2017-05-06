426eb92f5453facb03632278d7257df07ef217e4
Duncan X. Simpson <virtualdxs@gmail.com>
2017-05-05 21:35:10 -0700

    Fixed wizard power

--- src/com/cjd/textbaseddarksouls/Wizard.java
+++ src/com/cjd/textbaseddarksouls/Wizard.java
@@ -6,5 +6,8 @@ public class Wizard extends Player {
     public Wizard(String name) {
         super(name);
         POWER_MULTIPLIER = 5; //MAX_POWER = POWER_MULTIPLIER * level
+        MAX_POWER = POWER_MULTIPLIER;
+        power = MAX_POWER;
+        System.out.println(POWER_MULTIPLIER);
     }
 }
