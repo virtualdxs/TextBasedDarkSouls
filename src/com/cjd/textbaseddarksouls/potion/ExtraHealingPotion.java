009d95b207b4175153eaf16293ed07e775f0d386
Duncan X. Simpson <virtualdxs@gmail.com>
2017-05-07 12:53:30 -0700

    Fixed redefinition of BOOST_AMOUNT

--- src/com/cjd/textbaseddarksouls/potion/ExtraHealingPotion.java
+++ src/com/cjd/textbaseddarksouls/potion/ExtraHealingPotion.java
@@ -3,6 +3,9 @@ package com.cjd.textbaseddarksouls.potion;
 import static com.cjd.textbaseddarksouls.Master.*; //Give access to master context
 
 public class ExtraHealingPotion extends HealingPotion {
-  protected static int BOOST_AMOUNT = 25;
+  public ExtraHealingPotion() {
+    super();
+    BOOST_AMOUNT = 25;
+  }
   public String toString(){return "Extra Healing Potion";}
 }
