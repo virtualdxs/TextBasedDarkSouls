defde510d2cde587ad9efb2006bf5347a354008a
Duncan X. Simpson <virtualdxs@gmail.com>
2017-05-09 11:30:04 -0700

    Removed Boss.java (WIP on boss branch

--- src/com/cjd/textbaseddarksouls/Boss.java
+++
@@ -1,9 +0,0 @@
-package com.cjd.textbaseddarksouls;
-
-import static com.cjd.textbaseddarksouls.Master.*; //Give access to master context
-
-public interface Boss {
-  String getName();
-  int getHealth();
-  void dealDamage;
-}
