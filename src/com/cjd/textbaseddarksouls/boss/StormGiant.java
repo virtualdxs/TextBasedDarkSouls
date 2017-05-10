ca927c4248a9a7e48c90f04a490ce958aef1e66a
Cheri Pool <Cheri Pool>
2017-05-10 11:20:59 -0700

    Added the Drittsekk boss fight and changed defeat text for StormGiant

--- src/com/cjd/textbaseddarksouls/boss/StormGiant.java
+++ src/com/cjd/textbaseddarksouls/boss/StormGiant.java
@@ -4,14 +4,17 @@ import static com.cjd.textbaseddarksouls.Master.*; //Give access to master conte
 
 import com.cjd.textbaseddarksouls.Boss;
 
-public class StormGiant extends Boss {
-  private final String name = "Drittsekk the Storm Giant";
-  private int health = 200;
+public class StormGiant implements Boss {
+  private final String name = "Storm Giant";
+  private int health = 100;
 
   public String getName() { return name; }
   public int getHealth() { return health; }
   public void dealDamage(int damage) {
     if (damage >= health) onDeath();
     else health -= damage;
+  }
+  public void onDeath() {
+    System.out.println("I got five bucks on the Giant… Oh, you’re alive. I was totally counting on you winning. 100 Percent. All the way. Umm.");
   }
 }
