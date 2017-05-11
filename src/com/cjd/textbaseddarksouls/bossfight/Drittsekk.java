c8250ddc28aaf1b9118f7ef55b5b3e9e58304cea
Duncan X. Simpson <virtualdxs@gmail.com>
2017-05-11 07:25:56 -0700

    Fix syntax problems with StormGiant and Drittsekk

--- src/com/cjd/textbaseddarksouls/bossfight/Drittsekk.java
+++ src/com/cjd/textbaseddarksouls/bossfight/Drittsekk.java
@@ -3,9 +3,8 @@ package com.cjd.textbaseddarksouls.spell;
 import static com.cjd.textbaseddarksouls.Master.*; //Give access to master context
 
 import com.cjd.textbaseddarksouls.BossFight;
-
 import com.cjd.textbaseddarksouls.boss.StormGiant;
 
-public class Dritsekk implements BossFight{
+public class Drittsekk implements BossFight{
 
 }
