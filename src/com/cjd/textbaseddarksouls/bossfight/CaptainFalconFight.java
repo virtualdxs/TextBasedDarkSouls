203e2c070b8f9ceb91897a8b7c42f54b9063f00b
Duncan X. Simpson <virtualdxs@gmail.com>
2017-05-11 07:52:01 -0700

    Add bossfight classes

--- src/com/cjd/textbaseddarksouls/bossfight/CaptainFalconFight.java
+++ src/com/cjd/textbaseddarksouls/bossfight/CaptainFalconFight.java
@@ -3,8 +3,8 @@ package com.cjd.textbaseddarksouls.bossfight;
 import static com.cjd.textbaseddarksouls.Master.*; //Give access to master context
 
 import com.cjd.textbaseddarksouls.BossFight;
-import com.cjd.textbaseddarksouls.boss.StormGiant;
+import com.cjd.textbaseddarksouls.boss.CaptainFalcon;
 
-public class Drittsekk implements BossFight{
+public class CaptainFalconFight implements BossFight{
 
 }
---
+++ src/com/cjd/textbaseddarksouls/bossfight/DrittsekkFight.java
@@ -0,0 +1,10 @@
+package com.cjd.textbaseddarksouls.bossfight;
+
+import static com.cjd.textbaseddarksouls.Master.*; //Give access to master context
+
+import com.cjd.textbaseddarksouls.BossFight;
+import com.cjd.textbaseddarksouls.boss.StormGiant;
+
+public class DrittsekkFight implements BossFight{
+
+}
