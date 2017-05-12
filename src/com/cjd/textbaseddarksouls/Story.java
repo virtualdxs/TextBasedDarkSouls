bcabe34801f07eb3c0b012936fbb52ea46f15941
Duncan X. Simpson <virtualdxs@gmail.com>
2017-05-12 10:23:27 -0700

    Set up main method in story

--- src/com/cjd/textbaseddarksouls/Story.java
+++ src/com/cjd/textbaseddarksouls/Story.java
@@ -2,6 +2,8 @@ package com.cjd.textbaseddarksouls;
 
 import static com.cjd.textbaseddarksouls.Master.*; //Give access to master context
 
-public class Story {
-  
+public abstract class Story {
+  public static void main(String[] args) {
+    
+  }
 }
