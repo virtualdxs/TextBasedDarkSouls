b8525dac1c01de15a14739deaeb8b39d8416f301
Christian Martinez <alonsomartinez293@gmail.com>
2017-05-18 12:11:31 -0700

    Work on story

--- src/com/cjd/textbaseddarksouls/Story.java
+++ src/com/cjd/textbaseddarksouls/Story.java
@@ -69,5 +69,75 @@ public abstract class Story {
       default:
       throw new java.lang.IllegalArgumentException("Huh?");
     }
+    System.out.println("After hours of meandering about, you see an abandoned house that looks slightly more homely than the others. As you head inside, you hear a spooky sound rivet through the house.");
+    System.out.println("As you head inside, you hear a spooky sound rivet through the house. On the floor, you see two ghouls feasting on the corpse of an unknown creature.");
+    System.out.println("One of them snaps its head towards you, murder in its eyes. In a stupidly high pitched voice it exclaims “OMG, mooorrrreeeee foooood.” It then attacks you, as that is what ghouls do. The other one shouts “PELICAN, EEEEEEEEHHHHHHHHHH.”");
+    CombatContext combat2 = new CombatContext();
+    combat2.add(new Ghoul());
+    combat2.add(new Ghoul());
+    combat2.runCombat;
+    System.out.println("Wow! You killed them!");
+    System.out.println("The most fragile enemy you will encounter!");
+    System.out.println("You are a true hero.");
+    System.out.println("After that intense battle, you get to take a glance at your surroundings.");
+    System.out.println("1. Look around the house");
+    System.out.println("2. Search the ghouls");
+    System.out.println("3. Go upstairs");
+    System.out.println("4. Leave");
+
+    boolean done = false;
+    while(!done){
+      switch(getIntResponse(4)){
+        case 1:
+        System.out.println("There’s nothing. What’d you expect, a gargantuan treasure trove? You’re literally in some random peasant’s house.");
+        break;
+        case 2:
+        System.out.println("Really? The Ghouls? Alrighty then. You find… ABSOLUTELY NOTHING! Congratulations, you’ve disturbed the dead");
+        break;
+        case 3:
+        System.out.println("Well, nothing upstairs except for crushing darkness and the never ending expanse of your self-deprecating thoughts. Oh, and some gold or whatever.");
+        break;
+        case 4:
+        System.out.println("You exit the house and take another good look at your surroundings.");
+        done = true;
+        break;
+        default:
+        throw new java.lang.IllegalArgumentException("Huh?");
+        break;
+      }
+    }
+    System.out.println("As you observe the cool expanse of cobbled roads and darkened houses, you notice a rather large building in the center of town. You feel the compelling power of plot drawing you towards it. ");
+    System.out.println("As the almighty force of the plot being inflicted on you pushes you to the towering behemoth of a building, you see a clearly marked, neatly kept area labeled “CHECKPOINT”");
+    System.out.println("You continue on with your journey, towards the building you realize is a Cathedral, due to the amazing powers of sudden context. Inside the building is a immense throne, with a large shadowy figure settled upon it.");
+    System.out.println("Suddenly, for the purpose of advancing this story along at a rather quick pace, a bellowing voice booms from all around you:")
+    System.out.println("WHO DARES ENTER MY HOLY CATHEDRAL");
+    System.out.println("MY MOST HOLY OF PLACES, THE MOST SACRED PLACE TO EVER GRACE THIS MISERABLE REALM?");
+    System.out.println("WHO DARES COME TO THE HOME OF DRITTSEKK THE MIGHTY, DRITTSEKK THE POWERFUL");
+    System.out.println("DRITTSEKK THE CRUSHER OF CITIES");
+    System.out.println("THE DRINKER OF DRAGON BLOOD");
+    System.out.println("THE DESTROYER OF MAN");
+    System.out.println("THE ONLY GIANT TO EVER SUCCESSFULLY SPEEDRUN SUPER MARIO 64");
+    System.out.println("THE TENDER OF THE FIELD OF BATTLE");
+    System.out.println("THE MIGHTY RIDER OF VAN DU RA SEEL CAMME FORRU TASTS VELLU VAN GA RU THE GREAT WYRM");
+    System.out.println("THE ONLY FALCO MAIN OF MY CLAN");
+    System.out.println("EATER OF SOULS");
+    System.out.println("Man that guy can talk. Well, since he’ll be listing titles for about an hour, wanna donate to my college fund?");
+    System.out.println("1. Sure.");
+    System.out.println("2. Neck Yourself");
+    switch(getIntResponse(2)){
+      case 1:
+      System.out.println("Thanks, my paypal is www.paypal.com/IActuallyDontHaveAPaypal");
+      break;
+      case 2:
+      System.out.println("Alright, that's fine. I mean it’s not like I’ve been leading you through this adventure. ");
+      break;
+      default:
+      throw new java.lang.IllegalArgumentException("Huh?");
+      break;
+    }
+    System.out.println("Good luck on this next fight, by the way. Can I give you a bit of a tip? This guy is a boss, and not a hard one.");
+    System.out.println(" Even so, he could crush you beneath his foot like you were a spider. So, try not to die, okay?");
+
+    //BossFight drittsekk = new bossfight.Drittsekk();
   }
 }
